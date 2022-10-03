52d97d607c5fb5849a11e56648b5335879862d3c
AlessP93 <alespiscino@libero.it>
2022-10-03 17:05:28 +0200

    Fine esercizio

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -6,10 +6,10 @@
         public string Nome { get; set; }
         public string Description { get; set; }
         public string Pic { get; set; }
-        public string Price { get; set; }
+        public float Price { get; set; }
 
         //COSTRUTTORE
-        public Pizza(string nome, string description, string pic, string price)
+        public Pizza(string nome, string description, string pic, float price)
         {
             Nome = nome;
             Description = description;
